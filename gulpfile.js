212f942381918b3f71ac5008d9feaa67b1c6f27e
xwartz <stddup@gmail.com>
2015-10-22 23:39:22 +0800

    gulp file

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+
+var dirs = {
+  styles : './scss',
+  dest : './dist'
+};
+
+gulp.task('watch', function() {
+  gulp.watch(dirs.styles + '/**/*.scss', ['styles'])
+});
+
+gulp.task('styles', function() {
+  gulp.src(dirs.styles + '/cute.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 version'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(dirs.dest));
+});
